8d8441d69dc3581ebdba181b93daf67185339aa5
samar <sasa-2010-1995@hotmail.com>
2017-12-17 12:32:16 +0200

    remove console

--- frontend-src/fetch.js
+++ frontend-src/fetch.js
@@ -1,7 +1,6 @@
 const fetchPost = (method,url,cb,val) =>{
 const xhr = new XMLHttpRequest();
 xhr.onreadystatechange = function (){
-  console.log(xhr.readyState,xhr.status);
   if (xhr.readyState === 4 && xhr.status === 200) {
     cb(null,xhr.responseText);
   }
